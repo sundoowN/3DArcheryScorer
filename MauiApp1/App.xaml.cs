b14d26eeff55432a66da6cc53e6ba89453171859
sundowNuc <codywhittt@gmail.com>
2024-05-14 20:38:27 -0400

    Pushing again

--- MauiApp1/App.xaml.cs
+++ MauiApp1/App.xaml.cs
@@ -1,12 +1,14 @@
-﻿namespace MauiApp1
+﻿using MauiApp1.Pages;
+
+namespace MauiApp1
 {
     public partial class App : Application
     {
         public App()
         {
             InitializeComponent();
-
-            MainPage = new AppShell();
+            MainPage = new ArcheryHomePage();  
+            //MainPage = new AppShell();
         }
     }
 }
--- MauiApp1/obj/Debug/net8.0-maccatalyst/maccatalyst-x64/MauiApp1.AssemblyInfo.cs
+++ MauiApp1/obj/Debug/net8.0-maccatalyst/maccatalyst-x64/MauiApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MauiApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+8f134de16e635579b5941630724c0ce9e7100bfa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+aefc9d95d4ebb63b9b46de906a58bbcde6b9e5da")]
 [assembly: System.Reflection.AssemblyProductAttribute("MauiApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MauiApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
