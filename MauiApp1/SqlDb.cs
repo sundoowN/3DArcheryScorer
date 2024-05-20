52e5a127dbf3aaf9d8caf589b9cbeb2afd2bc350
Cody Whitaker <115599941+sundoowN@users.noreply.github.com>
2024-05-19 23:14:41 -0400

    Working on Score table

---
+++ MauiApp1/SqlDb.cs
@@ -0,0 +1,53 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiApp1
+{
+    public class Score
+    {
+        [PrimaryKey, AutoIncrement]
+        public string Target { get; set; }
+        public int JudgedDistance { get; set; }
+        public int ActualDistance { get; set; }
+        public int ScoringRing { get; set; }
+        public string Notes { get; set; }
+    }
+    public class SqlDb
+    {
+        static SQLiteAsyncConnection db; 
+        static async Task Init()
+        {
+            if (db != null)
+            {
+                return;
+            }
+            string DatabaseFilePath = Path.Combine(FileSystem.AppDataDirectory, "ScoringDatabase.db3");
+            db = new SQLiteAsyncConnection(DatabaseFilePath);
+            await db.CreateTableAsync<Score>();
+            var result = db.CreateTableAsync<Score>(); 
+        }
+        public async Task<List<Score>> GetAllScores()
+        {
+            await Init();
+            return await db.Table<Score>().ToListAsync();
+           
+        }
+        public async Task<int> AddScoreData(string target, int jd, int ad, int score, string notes)
+        {
+            await Init();
+            var data = new Score
+            {
+                Target = target,
+                JudgedDistance = jd,
+                ActualDistance = ad,
+                ScoringRing = score,
+                Notes = notes
+            };
+            return await db.InsertAsync(data);
+        }
+    }
+}
--- MauiApp1/ViewModel.cs
+++ MauiApp1/ViewModel.cs
@@ -11,12 +11,16 @@ namespace MauiApp1
     {
         public List<string> Targets { get; set; }
         public List<int> Scores { get; set; }
+        public List<int> JudgedYardage { get; set; }
+        public List<int> ActualYardage { get; set; }
         public string SelectedTarget { get; set; }
+        public SqlDb db; 
 
         public ViewModel()
         {
             AddTargetsToList(); 
-            AddScoresToPicker(); 
+            PopulateScoresPicker();
+            PopulateYardagesPickers(); 
         }
         public void AddTargetsToList()
         {
@@ -26,13 +30,25 @@ namespace MauiApp1
                 "Hyena", "Javelina", "Large Alert Deer", "Leopard", "Lynx", "Medium Deer", "Pronghorn Antelope", "Russian Boar",
                 "Turkey", "Wild Boar", "Wolf", "Wolverine", "XL Deer"];
         }
-        public void AddScoresToPicker()
+        public void PopulateScoresPicker()
         {
             Scores = [12, 10, 8, 5, 0];
+        }
+        public void PopulateYardagesPickers()
+        {
+            JudgedYardage = new List<int>();
+            ActualYardage = new List<int>(); 
+            for (int i = 0; i <= 60; i++)
+            {
+                JudgedYardage.Add(i);
+                ActualYardage.Add(i); 
+            }
         }
 
         public void AddScoreDataToFile(string json)
         {
+            db = new SqlDb();
+            //db.AddScoreData(); 
             var path = GetScoreDataFolderLocation();
             var dateAndTime = DateTime.Now;
             var date = dateAndTime.ToString("MM-dd-yy");
@@ -53,21 +69,24 @@ namespace MauiApp1
             var platform = DeviceInfo.Current;
             if (platform.Manufacturer.Equals("Apple"))
             {
-                var firstDir = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
+                //var firstDir = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
 
-                var assembly = typeof(App).GetTypeInfo().Assembly;
-                var loc = FileSystem.AppDataDirectory;
+                //var assembly = typeof(App).GetTypeInfo().Assembly;
                 var locs = FileSystem.Current.AppDataDirectory;
                 var pathsa = new DirectoryInfo(locs);
-                var application = pathsa.Parent;
-                var scorePath = application.ToString + @"\ScoreData"; 
-                var scoredatafolder = Directory.CreateDirectory(scorePath);  ; 
-
+                var file = Path.Combine(pathsa.FullName, "mytext.txt");
+                //var application = pathsa.Parent;
+                //var scorePath = application.Name + @"\ScoreData";  
+                //var scoredatafolder = Directory.CreateDirectory(scorePath); 
 
                 while (pathsa != null)
                 {
                     pathsa = pathsa.Parent;
-                    
+                    if (pathsa.ToString().Equals(@"/var/mobile/Containers/Data/Application"))
+                    {
+                        var scorePath = pathsa.ToString() + @"/ScoreData";
+                        var scoredatafolder = Directory.CreateDirectory(scorePath);
+                    }
                     var pathsa2 = Directory.GetDirectories(pathsa.ToString());
                 }
             } 
