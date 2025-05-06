7c6ad3c4bb75ed9ee91b9df0b7435843a790cd89
Cody Whitaker <codywhittt@gmail.com>
2025-05-05 20:26:12 -0500

    Pushing all changes.

--- MauiApp1/obj/Debug/net8.0-maccatalyst/maccatalyst-x64/MauiApp1.AssemblyInfo.cs
+++ MauiApp1/obj/Debug/net8.0-maccatalyst/maccatalyst-x64/MauiApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MauiApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+bb84ef15533023a9692e9311fee94c3747b4cbd4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+23affdb3673b40b2181e37ccd1a1a75d27acd9e5")]
 [assembly: System.Reflection.AssemblyProductAttribute("MauiApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MauiApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
